3150c5f59322e2f139d6c48d8a7b7454fc17f2a7
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-04-08 12:15:43 -0700

    test: Replace `spectator-v4` with `spectator-v5`

--- tests/Camille.RiotGames.Test/ApiComboFeaturedGamesSummonerCurrentGameV5Test.cs
+++ tests/Camille.RiotGames.Test/ApiComboFeaturedGamesSummonerCurrentGameV5Test.cs
@@ -3,25 +3,25 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Camille.Enums;
-using Camille.RiotGames.SpectatorV4;
+using Camille.RiotGames.SpectatorV5;
 using System;
 
 namespace Camille.RiotGames.Test
 {
     [TestClass]
-    public class ApiComboFeaturedGamesSummonerCurrentGameV4Test : ApiTest
+    public class ApiComboFeaturedGamesSummonerCurrentGameV5Test : ApiTest
     {
         public const PlatformRoute REGION = PlatformRoute.EUW1;
 
         [TestMethod]
         public void Get()
         {
-            var featured = Api.SpectatorV4().GetFeaturedGames(REGION);
+            var featured = Api.SpectatorV5().GetFeaturedGames(REGION);
             CheckFeatured(featured);
             foreach (var gameInfo in featured.GameList)
             {
                 var participant = gameInfo.Participants[0];
-                var currentGame = Api.SpectatorV4().GetCurrentGameInfoBySummoner(REGION, participant.SummonerId);
+                var currentGame = Api.SpectatorV5().GetCurrentGameInfoByPuuid(REGION, participant.Puuid);
                 Assert.IsNotNull(currentGame);
                 Assert.AreEqual(gameInfo.GameId, currentGame.GameId);
                 Assert.IsTrue(currentGame.Participants.Any(cp => participant.SummonerName.Equals(cp.SummonerName)),
@@ -32,12 +32,12 @@ namespace Camille.RiotGames.Test
         [TestMethod]
         public async Task GetAsync()
         {
-            var featured = await Api.SpectatorV4().GetFeaturedGamesAsync(REGION);
+            var featured = await Api.SpectatorV5().GetFeaturedGamesAsync(REGION);
             CheckFeatured(featured);
             var tasks = featured.GameList.Select(async gameInfo =>
             {
                 var participant = gameInfo.Participants[0];
-                var currentGame = await Api.SpectatorV4().GetCurrentGameInfoBySummonerAsync(REGION, participant.SummonerId);
+                var currentGame = await Api.SpectatorV5().GetCurrentGameInfoByPuuidAsync(REGION, participant.Puuid);
                 Assert.IsNotNull(currentGame);
                 Assert.AreEqual(gameInfo.GameId, currentGame.GameId);
                 Assert.IsTrue(currentGame.Participants.Any(cp => participant.SummonerName.Equals(cp.SummonerName)),
@@ -49,12 +49,12 @@ namespace Camille.RiotGames.Test
         [TestMethod]
         public void GetParallel()
         {
-            var featured = Api.SpectatorV4().GetFeaturedGames(REGION);
+            var featured = Api.SpectatorV5().GetFeaturedGames(REGION);
             CheckFeatured(featured);
             var result = Parallel.ForEach(featured.GameList, gameInfo =>
             {
                 var participant = gameInfo.Participants[0];
-                var currentGame = Api.SpectatorV4().GetCurrentGameInfoBySummoner(REGION, participant.SummonerId);
+                var currentGame = Api.SpectatorV5().GetCurrentGameInfoByPuuid(REGION, participant.Puuid);
                 Assert.IsNotNull(currentGame);
                 Assert.AreEqual(gameInfo.GameId, currentGame.GameId);
                 Assert.IsTrue(currentGame.Participants.Any(cp => participant.SummonerName.Equals(cp.SummonerName)),
